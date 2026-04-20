748ea7ccbe5a1672e3708a2702492db2c89ede31
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-04-20 10:04:54 +0700

    chore(user): add AppColors class for consistent color management

---
+++ user/lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryDark = Color(0xFF1A3C15); // Hijau tua di tombol
+  static const Color textBlack = Color(0xFF1E1E1E);
+  static const Color textBrown = Color(0xFF4A2C2A); // Warna "Discovery"
+  static const Color greyLight = Color(0xFFF5F5F5); // Background kartu
+}
