5ee87dc744246980fa8e6f4e6b4c689fc283dc3e
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-05-09 23:32:40 +0700

    chore(user): update AppColors class with additional color constants

--- user/lib/core/constants/app_colors.dart
+++ user/lib/core/constants/app_colors.dart
@@ -4,5 +4,9 @@ class AppColors {
   static const Color primaryDark = Color(0xFF1A3C15); // Hijau tua di tombol
   static const Color textBlack = Color(0xFF1E1E1E);
   static const Color textBrown = Color(0xFF4A2C2A); // Warna "Discovery"
-  static const Color greyLight = Color(0xFFF5F5F5); // Background kartu
+  static const Color textGold = Color(0xFFD4AF37); // Warna "Selamat Datang"
+  static const Color greyLight = Color(0xFFF5F5F5);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color orange = Color(0xFFFF8C42); // Accent color
+  static const Color textGrey = Color(0xFF666666);
 }
