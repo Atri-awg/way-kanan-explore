4330661ada1adfccdacc50739cb2082c4b0e39a6
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-05-09 23:45:44 +0700

    chore(user): add asset paths for hero image and tourist destinations

--- user/lib/core/constants/app_assets.dart
+++ user/lib/core/constants/app_assets.dart
@@ -1,3 +1,8 @@
 class AppAssets {
   static const String logo = "assets/images/WayKananExploreLogo.png";
+  static const String heroImage = "assets/images/hero_waterfall.jpg";
+  static const String destination1 = "assets/images/air_terjun_tirta_haji.jpg";
+  static const String destination2 = "assets/images/sungai_way_besai.jpg";
+  static const String destination3 = "assets/images/bukit_barisan.jpg";
+  static const String destination4 = "assets/images/goa_kelelawar.jpg";
 }
