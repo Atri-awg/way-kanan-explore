6846e07d2c3406bb538c9cbfdf664cad6ca5b77c
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2026-06-07 00:20:48 +0700

    dto destinasi

--- api/destinasi-service/src/destinasi/dto/create-destinasi.dto.ts
+++ api/destinasi-service/src/destinasi/dto/create-destinasi.dto.ts
@@ -1,15 +1,36 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  IsBoolean,
+} from 'class-validator';
+
 export class CreateDestinasiDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
+  @IsNotEmpty()
+  @IsString()
   slug: string;
 
+  @IsNotEmpty()
+  @IsString()
   description: string;
 
+  @IsNotEmpty()
+  @IsString()
   location: string;
 
-  categoryId: string;
-
+  @IsOptional()
+  @IsString()
   thumbnail?: string;
 
+  @IsNotEmpty()
+  @IsString()
+  categoryId: string;
+
+  @IsOptional()
+  @IsBoolean()
   status?: boolean;
 }
