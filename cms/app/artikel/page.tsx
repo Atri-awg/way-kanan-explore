023698170869131dd62543b645e57808e335ab64
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2026-06-06 16:47:00 +0700

    membuat halaman tambah artikel

--- cms/app/artikel/page.tsx
+++ cms/app/artikel/page.tsx
@@ -17,7 +17,7 @@ export default function ArtikelPage() {
         </div>
 
         <Link
-          href="/dashboard/artikel/tambah"
+          href="/artikel/tambah"
           className="bg-green-600 text-white px-4 py-2 rounded-lg flex items-center gap-2"
         >
           <Plus size={18} />
