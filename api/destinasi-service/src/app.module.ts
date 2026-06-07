edf475e27ea3db63c9bfecf1017f1e2c41ee81c1
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2026-06-07 14:53:32 +0700

    tmbah prisma service

--- api/destinasi-service/src/app.module.ts
+++ api/destinasi-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DestinasiModule } from './destinasi/destinasi.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [DestinasiModule],
+  imports: [DestinasiModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
