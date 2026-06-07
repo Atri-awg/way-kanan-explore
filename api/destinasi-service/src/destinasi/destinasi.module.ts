bd53cee6284f5aada07e3e7523f7f3c11cc64c79
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2026-06-07 15:04:38 +0700

    import prisma module ke destinasi module

--- api/destinasi-service/src/destinasi/destinasi.module.ts
+++ api/destinasi-service/src/destinasi/destinasi.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DestinasiService } from './destinasi.service';
 import { DestinasiController } from './destinasi.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [DestinasiController],
   providers: [DestinasiService],
 })
