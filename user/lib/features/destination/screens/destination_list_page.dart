11e57249f04b05e58ab574392ba965a0e8cd0c38
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-06-13 00:52:09 +0700

    feat: tambah DestinationListPage untuk tampilkan list destinations

---
+++ user/lib/features/destination/screens/destination_list_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:user/features/destination/models/destination_model.dart';
+import 'package:user/features/destination/services/destination_service.dart';
+import 'package:user/features/home/widgets/destination_card.dart';
+import 'destination_detail_page.dart';
+
+class DestinationListPage extends StatelessWidget {
+  DestinationListPage({super.key});
+
+  final DestinationService service = DestinationService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Destinations'),
+      ),
+      body: FutureBuilder<List<Destination>>(
+        future: service.getDestinations(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState ==
+              ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          final destinations = snapshot.data ?? [];
+
+          return ListView.builder(
+            itemCount: destinations.length,
+            itemBuilder: (context, index) {
+              final destination = destinations[index];
+
+              return DestinationCard(
+                destination: destination,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => DestinationDetailPage(
+                        destination: destination,
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+  
+}
