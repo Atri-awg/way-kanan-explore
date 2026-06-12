5bdf20a4b4cb9025aa1e1d43b2340e4eb1dcb116
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2026-06-12 15:59:56 +0700

    membuat file conflict kategori

---
+++ api/kategori/src/common/utils/conflict-kategori.util.ts
@@ -0,0 +1,37 @@
+import { ConflictException, HttpStatus } from '@nestjs/common';
+import { PrismaService } from '../../prisma.service';
+// buat fungsi untuk cek
+// duplikasi data kategori
+export const conflictKategori = async (
+  prisma: PrismaService['kategori'],
+  message: string,
+  nama: string,
+  id?: number,
+) => {
+  // buat variabel filter nama
+  const nama_filter = nama.replace(/\s/g, '').toLowerCase().trim();
+
+  // cek apakah nama kategori sudah ada
+  const exist = await prisma.findFirst({
+    where: {
+      // ternary operator
+      //NOT: id ? { id: id } : undefined,
+      nama_filter: nama_filter,
+      // spread operator
+      ...(id ? { NOT: { id: id } } : undefined),
+    },
+  });
+
+  // jika nama kategori sudah ada
+  if (exist) {
+    throw new ConflictException({
+      success: false,
+      message: message,
+      metadata: {
+        status: HttpStatus.CONFLICT,
+      },
+    });
+  }
+
+  return nama_filter;
+};
