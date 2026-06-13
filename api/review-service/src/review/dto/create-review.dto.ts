188ee171ef608a4af65b54ae241a72c6803b63bb
AzrRja <alfiazariaroja771@gmail.com>
2026-06-13 21:31:41 +0700

    refactor: reorganize imports and format CreateReviewDto class

--- api/review-service/src/review/dto/create-review.dto.ts
+++ api/review-service/src/review/dto/create-review.dto.ts
@@ -1,7 +1,17 @@
-import {
-  IsInt,
-  IsString,
-  Max,
-  Min,
-} from 'class-validator';
+import { IsInt, IsString, Max, Min } from 'class-validator';
 
+export class CreateReviewDto {
+  @IsString()
+  destinationId!: string;
+
+  @IsString()
+  reviewerName!: string;
+
+  @IsString()
+  comment!: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating!: number;
+}
