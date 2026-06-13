a17814c70c57d569efa63d73e585c7d8e00ecccd
AzrRja <alfiazariaroja771@gmail.com>
2026-06-13 21:26:39 +0700

    feat: enhance main.ts with CORS support and global validation pipes

--- api/review-service/src/main.ts
+++ api/review-service/src/main.ts
@@ -1,9 +1,20 @@
 import 'reflect-metadata';
+import {ValidationPipe} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 }
-bootstrap();
+void bootstrap();
