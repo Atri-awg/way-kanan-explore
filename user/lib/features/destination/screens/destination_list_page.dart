13a1376df74ceb6bdcc281ec45fa549c4a3f97d9
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-06-13 21:03:20 +0700

    feat: refactor DestinationListPage to StatefulWidget and implement destination loading and searching

--- user/lib/features/destination/screens/destination_list_page.dart
+++ user/lib/features/destination/screens/destination_list_page.dart
@@ -4,12 +4,50 @@ import 'package:user/features/destination/services/destination_service.dart';
 import 'package:user/features/home/widgets/destination_card.dart';
 import 'destination_detail_page.dart';
 
-class DestinationListPage extends StatelessWidget {
+class DestinationListPage extends StatefulWidget {
   DestinationListPage({super.key});
 
+  @override
+  State<DestinationListPage> createState() => _DestinationListPageState();
+}
+
+class _DestinationListPageState extends State<DestinationListPage> {
   final DestinationService service = DestinationService();
 
+  List<Destination> allDestinations = [];
+  List<Destination> filteredDestinations = [];
+
+  bool isLoading = true;
+
+  final TextEditingController searchController =
+      TextEditingController();
+
   @override
+  void initState() {
+    super.initState();
+    loadDestinations();
+  }
+
+  Future<void> loadDestinations() async {
+    final data = await service.getDestinations();
+
+    setState(() {
+      allDestinations = data;
+      filteredDestinations = data;
+      isLoading = false;
+    });
+  }
+
+  void searchDestination(String keyword) {
+    setState(() {
+      filteredDestinations =
+          allDestinations.where((destination) {
+        return destination.name
+            .toLowerCase()
+            .contains(keyword.toLowerCase());
+      }).toList();
+    });
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Destinations')),
@@ -33,14 +71,16 @@ class DestinationListPage extends StatelessWidget {
           if (snapshot.hasError) {
             return Center(child: Text(snapshot.error.toString()));
           }
-          
 
           final destinations = snapshot.data ?? [];
           print(destinations.first.name);
           print(destinations.first.toJson());
 
           return ListView.separated(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 12.0,
+            ),
             itemCount: destinations.length,
             itemBuilder: (context, index) {
               final destination = destinations[index];
