87b4a53953cb64c4581c5842229a4cad69b4fbda
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-06-13 23:46:52 +0700

    feat: update main.dart to include additional feature imports and set HomePage as the default home

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-// import 'package:user/features/home/screens/home_page.dart';
-// import 'package:user/features/travel_list/travel_list_page.dart';
-// import 'package:user/features/destination/screens/destination_detail_page.dart';
+import 'package:user/features/home/screens/home_page.dart';
+import 'package:user/features/travel_list/travel_list_page.dart';
+import 'package:user/features/destination/screens/destination_detail_page.dart';
 import 'package:user/features/destination/screens/destination_list_page.dart';
+
+import 'package:user/features/about/screens/about_way_kanan_page.dart';
+import 'package:user/features/contact/screens/contact_page.dart';
+import 'package:user/features/gallery/screens/gallery_page.dart';
+import 'package:user/features/destination/screens/destination_list_page.dart';
+import 'package:user/features/article/screens/article_page.dart';
 
 
 void main() {
@@ -16,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DestinationListPage(),
+      home: HomePage(),
       // theme: ThemeData(
       //   useMaterial3: true,
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
