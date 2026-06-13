c5615c6da4e5e098e9ec5b34ec10efa20b126be9
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-06-13 23:47:00 +0700

    feat: clean up main.dart by removing unused imports and commented-out theme code

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:user/features/home/screens/home_page.dart';
-import 'package:user/features/travel_list/travel_list_page.dart';
-import 'package:user/features/destination/screens/destination_detail_page.dart';
-import 'package:user/features/destination/screens/destination_list_page.dart';
 
-import 'package:user/features/about/screens/about_way_kanan_page.dart';
-import 'package:user/features/contact/screens/contact_page.dart';
-import 'package:user/features/gallery/screens/gallery_page.dart';
-import 'package:user/features/destination/screens/destination_list_page.dart';
-import 'package:user/features/article/screens/article_page.dart';
 
 
 void main() {
@@ -23,16 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-      // theme: ThemeData(
-      //   useMaterial3: true,
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-
-      //   appBarTheme: const AppBarTheme(
-      //     backgroundColor: Colors.deepPurple,
-      //     foregroundColor: Colors.white,
-      //     centerTitle: true,
-      //   ),
-      // ),
     );
   }
 }
