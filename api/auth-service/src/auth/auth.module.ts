2edf56cb1528575718a021531a7e48660bbd21e4
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2026-06-13 15:56:21 +0700

    membuat file guards dan strategy

--- api/auth-service/src/auth/auth.module.ts
+++ api/auth-service/src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
@@ -13,7 +16,7 @@ import { PrismaService } from '../prisma/prisma.service';
       },
     }),
   ],
+  providers: [AuthService, PrismaService, JwtStrategy],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
