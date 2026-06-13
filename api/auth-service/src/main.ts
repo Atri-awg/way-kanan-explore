97db93dcbc1bc687cec18ae478002974eb8f6449
defriliyadi8 <defri_liyadi@teknokrat.ac.id>
2026-06-13 15:26:24 +0700

    downl;oad config dan set up main ts

--- api/auth-service/src/main.ts
+++ api/auth-service/src/main.ts
@@ -1,8 +1,14 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(3001);
+
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+  console.log(`🚀 Auth Service running on http://localhost:3001`);
 }
 bootstrap();
