221fa01cf63dfc25e269da59c76925cd300e0b0b
AzrRja <alfiazariaroja771@gmail.com>
2026-06-13 23:14:10 +0700

    refactor: restructure NotificationController with complete CRUD operations

--- api/notification-service/src/notification/notification.controller.spec.ts
+++ api/notification-service/src/notification/notification.controller.spec.ts
@@ -1,18 +1,80 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { NotificationController } from './notification.controller';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-describe('NotificationController', () => {
-  let controller: NotificationController;
+import { NotificationService } from './notification.service';
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [NotificationController],
-    }).compile();
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { QueryNotificationDto } from './dto/query-notification.dto';
 
-    controller = module.get<NotificationController>(NotificationController);
-  });
+@Controller('notification')
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+  @Post()
+  create(
+    @Body()
+    createNotificationDto: CreateNotificationDto,
+  ) {
+    return this.notificationService.create(createNotificationDto);
+  }
+
+  @Get()
+  findAll(
+    @Query()
+    query: QueryNotificationDto,
+  ) {
+    return this.notificationService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id')
+    id: string,
+  ) {
+    return this.notificationService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id')
+    id: string,
+
+    @Body()
+    updateNotificationDto: UpdateNotificationDto,
+  ) {
+    return this.notificationService.update(id, updateNotificationDto);
+  }
+
+  @Patch(':id/read')
+  markAsRead(
+    @Param('id')
+    id: string,
+  ) {
+    return this.notificationService.markAsRead(id);
+  }
+
+  @Delete(':id')
+  remove(
+    @Param('id')
+    id: string,
+  ) {
+    return this.notificationService.remove(id);
+  }
+
+  @Patch(':id/restore')
+  restore(
+    @Param('id')
+    id: string,
+  ) {
+    return this.notificationService.restore(id);
+  }
+}
