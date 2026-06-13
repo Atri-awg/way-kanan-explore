1c6ec5e6f6f8e536c252ab0f2e531934e5519a4c
AzrRja <alfiazariaroja771@gmail.com>
2026-06-13 22:04:55 +0700

    feat: enhance ReviewService with necessary imports for review handling

--- api/review-service/src/review/review.service.ts
+++ api/review-service/src/review/review.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+
+import { Prisma, Review } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+import { CreateReviewDto } from './dto/create-review.dto';
+import { UpdateReviewDto } from './dto/update-review.dto';
+import { QueryReviewDto } from './dto/query-review.dto';
 
 @Injectable()
 export class ReviewService {}
