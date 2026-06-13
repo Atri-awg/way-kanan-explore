dc18e87d86f77e0af8edaddec2f0ab5dd3ff6256
AzrRja <alfiazariaroja771@gmail.com>
2026-06-13 22:15:19 +0700

    feat: implement ReviewController with CRUD operations for reviews

--- api/review-service/src/review/review.controller.ts
+++ api/review-service/src/review/review.controller.ts
@@ -1,4 +1,80 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+import { ReviewService } from './review.service';
+
+import { CreateReviewDto } from './dto/create-review.dto';
+import { UpdateReviewDto } from './dto/update-review.dto';
+import { QueryReviewDto } from './dto/query-review.dto';
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
+  @Post()
+  create(
+    @Body()
+    createReviewDto: CreateReviewDto,
+  ) {
+    return this.reviewService.create(createReviewDto);
+  }
+
+  @Get()
+  findAll(
+    @Query()
+    query: QueryReviewDto,
+  ) {
+    return this.reviewService.findAll(query);
+  }
+
+  @Get('destination/:destinationId/rating')
+  getAverageRating(
+    @Param('destinationId')
+    destinationId: string,
+  ) {
+    return this.reviewService.getAverageRating(destinationId);
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id')
+    id: string,
+  ) {
+    return this.reviewService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id')
+    id: string,
+
+    @Body()
+    updateReviewDto: UpdateReviewDto,
+  ) {
+    return this.reviewService.update(id, updateReviewDto);
+  }
+
+  @Delete(':id')
+  remove(
+    @Param('id')
+    id: string,
+  ) {
+    return this.reviewService.remove(id);
+  }
+
+  @Patch(':id/restore')
+  restore(
+    @Param('id')
+    id: string,
+  ) {
+    return this.reviewService.restore(id);
+  }
+}
